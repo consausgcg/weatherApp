64c9cabdd89e4ac148ab6de38fd5f559cba1296a
Aus Gomez <austreberto.gomez@capgroup.com>
2021-04-08 10:15:34 -0500

    Initial Commit

---
+++ weatherApp/weatherAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  weatherAppApp.swift
+//  weatherApp
+//
+//  Created by Austreberto Gomez on 4/8/21.
+//
+
+import SwiftUI
+
+@main
+struct weatherAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
